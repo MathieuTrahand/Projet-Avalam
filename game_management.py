caea89b5593c388740826945b3e96060c9b9c0c1
Mathieu <mathieu.trahand@gmail.com>
2024-01-29 13:15:05 +0100

    class button

--- game_management.py
+++ game_management.py
@@ -6,6 +6,35 @@ empty_surface = pygame.Surface((0, 0))
 pawns_images = {}
 
 
+class Button:
+    def __init__(self, x, y, image, scale):
+        width = image.get_width()
+        height = image.get_height()
+        self.image = pygame.transform.scale(image, (int(width * scale), int(height * scale)))
+        self.rect = self.image.get_rect()
+        self.rect.topleft = (x, y)
+        self.clicked = False
+
+    def draw(self, surface):
+        action = False
+        # get mouse position
+        pos = pygame.mouse.get_pos()
+
+        # check mouseover and clicked conditions
+        if self.rect.collidepoint(pos):
+            if pygame.mouse.get_pressed()[0] == 1 and self.clicked == False:
+                self.clicked = True
+                action = True
+
+        if pygame.mouse.get_pressed()[0] == 0:
+            self.clicked = False
+
+        # draw button on screen
+        surface.blit(self.image, (self.rect.x, self.rect.y))
+
+        return action
+
+
 class Player:
     def __init__(self, name, all_piles, color="blanc"):
         self.name = name
@@ -44,7 +73,6 @@ class PawnsPile:
         self.all_piles = all_piles
         self.image = self.load_image()
         self.rect = self.image.get_rect(topleft=self.initial_position)
-
 
     def load_image(self):
         if self.nb_pawns > 0:
